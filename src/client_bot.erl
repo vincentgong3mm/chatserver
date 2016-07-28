d53d38909b36d4da39e9c43b8387df44245240d9
SeongBaeGong <vincentgong3mm@gmail.com>
2016-07-28 18:16:08 +0900

    client bot에 id추가

--- src/client_bot.erl
+++ src/client_bot.erl
@@ -13,17 +13,29 @@
     connect_and_send/0
 ]).
 
+-record(state, {
+    socket, 
+    room_name,
+    user_name 
+    }).
 
 
-start_link(Temp) ->
+
+start_link(RoomNum) ->
     ?LOG(client_bot),
     gen_server:start_link({local, ?MODULE},    % Process Name
                         ?MODULE,  % Module 
-                        [], % Arg
+                        [RoomNum], % Arg
                         []).    % Opt
     
-init([]) ->
-    State = 0,
+init([RoomNum]) ->
+    RoomName = lists:concat(["chatRoom", RoomNum]),
+    UserName = lists:concat(["user", RoomNum]),
+
+    %State = [RoomName, UserName],
+    State = #state{socket = 0, room_name = RoomName, user_name = UserName},
+    ?LOG(State),
+
     {ok, State}.
 terminate(_Reason, _State) ->
     ok.
@@ -33,19 +45,45 @@ terminate(_Reason, _State) ->
 %    State2 = Socket,    
 %    {reply, _From, State2};
 handle_call({login}, _From, State) ->
+    ?LOG(State),
+    %Packet = unicode:characters_to_binary("/login:xxx:name100"),
+    [RoomName, UserName] = State,
 
-    Packet = unicode:characters_to_binary("/login:xxx:name100"),
+    Str = lists:concat(["/login:xxx:", UserName]),
+    Packet = unicode:characters_to_binary(Str),
 
     gen_tcp:send(State, Packet),
     {reply, _From, State};
 handle_call({create}, _From, State) ->
-    gen_tcp:send(State, <<"/create:room100:0000">>),
+    %gen_tcp:send(State, <<"/create:room100:0000">>),
+    [RoomName, UserName] = State,
+
+    Str = lists:concat(["/create:", RoomName, ":xxxx"]),
+    Packet = unicode:characters_to_binary(Str),
+
+    gen_tcp:send(State, Packet),
+
     {reply, _From, State};
 handle_call({join}, _From, State) ->
-    gen_tcp:send(State, <<"/join:room100:xxxx">>),
+    %gen_tcp:send(State, <<"/join:room100:xxxx">>),
+    [RoomName, UserName] = State,
+
+    Str = lists:concat(["/join:", RoomName, ":xxxx"]),
+    Packet = unicode:characters_to_binary(Str),
+
+    gen_tcp:send(State, Packet),
+
     {reply, _From, State};
 handle_call({chat}, _From, State) ->
-    gen_tcp:send(State, <<"/chat:room100:testmessage">>),
+    %gen_tcp:send(State, <<"/chat:room100:testmessage">>),
+
+    [RoomName, UserName] = State,
+
+    Str = lists:concat(["/chat:", RoomName, ":test----chat----", UserName]),
+    Packet = unicode:characters_to_binary(Str),
+
+    gen_tcp:send(State, Packet),
+
     {reply, _From, State};
 
 handle_call({_Value, Temp}, _From, State) ->
@@ -57,20 +95,63 @@ handle_cast({cl}, State) ->
       
 handle_cast({connect}, State) ->
     {ok, Socket} = gen_tcp:connect({127,0,0,1}, 8080, [binary, {active,true}]),
-    State2 = Socket,    
+
+    State2 = State#state{socket = Socket},
     {noreply, State2};
 handle_cast({login}, State) ->
-    gen_tcp:send(State, "/login:xxx:name100\r\n"),
+    ?LOG(State),
+    RoomName = State#state.room_name,
+    UserName = State#state.user_name,
+    Socket = State#state.socket,
+
+    Str = lists:concat(["/login:xxx:", UserName]),
+
+    ?LOG(Str),
+
+    Packet = unicode:characters_to_binary(Str),
+
+    ?LOG(Packet),
+
+    gen_tcp:send(Socket, Packet),
 
     {noreply, State};
 handle_cast({create}, State) ->
-    gen_tcp:send(State, <<"/create:room100:0000\r\n">>),
+    %gen_tcp:send(State, <<"/create:room100:0000\r\n">>),
+
+    RoomName = State#state.room_name,
+    UserName = State#state.user_name,
+    Socket = State#state.socket,
+
+
+    Str = lists:concat(["/create:", RoomName, ":xxxx"]),
+    Packet = unicode:characters_to_binary(Str),
+
+    gen_tcp:send(Socket, Packet),
     {noreply, State};
 handle_cast({join}, State) ->
-    gen_tcp:send(State, <<"/join:room100:xxxx\r\n">>),
+    %gen_tcp:send(State, <<"/join:room100:xxxx\r\n">>),
+    RoomName = State#state.room_name,
+    UserName = State#state.user_name,
+    Socket = State#state.socket,
+
+    Str = lists:concat(["/join:", RoomName, ":xxxx"]),
+    Packet = unicode:characters_to_binary(Str),
+
+    gen_tcp:send(Socket, Packet),
+
     {noreply, State};
 handle_cast({chat}, State) ->
-    gen_tcp:send(State, <<"/chat:room100:testmessage\r\n">>),
+    %gen_tcp:send(State, <<"/chat:room100:testmessage\r\n">>),
+
+    RoomName = State#state.room_name,
+    UserName = State#state.user_name,
+    Socket = State#state.socket,
+
+    Str = lists:concat(["/chat:", RoomName, ":test----chat----", UserName]),
+    Packet = unicode:characters_to_binary(Str),
+
+    gen_tcp:send(Socket, Packet),
+
     {noreply, State};
 
 
